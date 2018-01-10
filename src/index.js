6f2bdd67d0dfef8311aa7050695862cb91bb2bd4
Caleb Bassi <calebjbassi@gmail.com>
2018-01-09 22:08:26 -0800

    Fixed unused variable warning

--- src/index.js
+++ src/index.js
@@ -100,7 +100,7 @@ class Game extends React.Component {
   render() {
     const history = this.state.history
     const current = history[this.state.stepNumber]
-    const [winner, lines] = calculateWinner(current.squares)
+    const winner = calculateWinner(current.squares)[0]
     let moves = history.map((step, move) => {
       const desc = move ?
         `Go to move #${move} (${this.state.cellClicked[move]})`:
@@ -149,7 +149,7 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById("root"))
+ReactDOM.render(<Game />, document.getElementById('root'))
 
 function calculateWinner(squares) {
   const lines = [
